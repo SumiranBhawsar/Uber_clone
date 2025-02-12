bf7b70048eedb955f1092f3ab6029b97223926de
Sumiran Bhawsar <bhawsarsumiran149@gmail.com>
2025-02-13 01:20:21 +0530

    add

--- backend/src/routes/captain.routes.js
+++ backend/src/routes/captain.routes.js
@@ -5,19 +5,28 @@ import { authentication } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(
-    [
-        body("fullname").notEmpty().withMessage("Fullname is required"),
-        body("email").isEmail().withMessage("Valid email is required"),
-        body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
-        body("model").notEmpty().withMessage("Vehicle model is required"),
-        body("color").notEmpty().withMessage("Vehicle color is required"),
-        body("vehicleType").notEmpty().withMessage("Vehicle type is required"),
-        body("licensePlate").notEmpty().withMessage("License plate is required"),
-        body("capacity").isInt({ min: 1 }).withMessage("Capacity must be at least 1"),
-    ],
-    captainRegister
-);
+router
+    .route("/register")
+    .post(
+        [
+            body("fullname").notEmpty().withMessage("Fullname is required"),
+            body("email").isEmail().withMessage("Valid email is required"),
+            body("password")
+                .isLength({ min: 6 })
+                .withMessage("Password must be at least 6 characters long"),
+            body("model").notEmpty().withMessage("Vehicle model is required"),
+            body("color").notEmpty().withMessage("Vehicle color is required"),
+            body("vehicleType")
+                .notEmpty()
+                .withMessage("Vehicle type is required"),
+            body("licensePlate")
+                .notEmpty()
+                .withMessage("License plate is required"),
+            body("capacity")
+                .isInt({ min: 1 })
+                .withMessage("Capacity must be at least 1"),
+        ],
+        captainRegister
+    );
 
 export default router;
-
