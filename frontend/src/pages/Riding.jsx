bea73648b6c08e485e637a3d06d7c9fe8c265034
Sumiran Bhawsar <bhawsarsumiran149@gmail.com>
2025-04-01 11:34:13 +0530

    changes in riding page

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -6,7 +6,10 @@ import { RiHome2Line } from "react-icons/ri";
 function Riding() {
   return (
     <div className="w-full h-screen">
-      <Link className="absolute text-2xl bg-white p-2 rounded-full right-4 top-4" to={"/home"}>
+      <Link
+        className="absolute text-2xl bg-white p-2 rounded-full right-4 top-4"
+        to={"/home"}
+      >
         <RiHome2Line />
       </Link>
       <div className="h-screen w-full">
@@ -17,15 +20,25 @@ function Riding() {
         />
       </div>
       <div className="fixed z-10 bottom-0 bg-white w-full px-4 py-2">
-        <div className="w-full flex items-center justify-center">
-          <div className="h-20 w-44 rounded-full bg-sky-50 flex items-center justify-center mt-8">
-            <div className="h-10 w-36 bg-sky-100 rounded-full">
-              <img
-                className="absolute top-2 left-24 h-24"
-                src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png"
-                alt=""
-              />
-            </div>
+        <div className="w-full flex items-center justify-between py-2">
+          <div className="flex relative">
+            <img
+              className="h-18 w-18 absolute mt-1 rounded-full object-cover"
+              src="https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?q=80&w=1966&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+            <img
+              className="h-18 ml-4 bg-sky-50 rounded-full mt-1"
+              src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png"
+              alt=""
+            />
+          </div>
+          <div className="text-right">
+            <h4 className="font-medium">Sumiran Bhawsar</h4>
+            <h2 className="font-bold text-lg">MP12 XY6264</h2>
+            <p className="text-sm font-medium text-gray-600">
+              White Suzuki S-Perso LXI
+            </p>
           </div>
         </div>
         <div className="w-full mt-4">
